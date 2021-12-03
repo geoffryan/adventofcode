cb635f79d1fedd3c5b4aab05070cfab560c93725
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2021-12-03 15:02:22 -0500

    Done day2!

--- 2021/day02/src/main.rs
+++ 2021/day02/src/main.rs
@@ -8,6 +8,32 @@ enum Leg {
     Forward(i64)
 }
 
+#[derive(Debug)]
+struct Position {
+    depth: i64,
+    horizontal: i64,
+    aim: i64,
+}
+
+impl Position {
+    fn mov_bad(&mut self, leg: &Leg) {
+        match leg {
+            Leg::Up(x) => self.depth -= x,
+            Leg::Down(x) => self.depth += x,
+            Leg::Forward(x) => self.horizontal += x,
+        }
+    }
+
+    fn mov(&mut self, leg: &Leg) {
+        match leg {
+            Leg::Up(x) => self.aim -= x,
+            Leg::Down(x) => self.aim += x,
+            Leg::Forward(x) => {self.horizontal += x;
+                                self.depth += self.aim * x},
+        }
+    }
+}
+
 fn str_to_path(input: &str) -> Leg {
     let words: Vec<&str> = input.split(' ').collect();
 
@@ -36,9 +62,17 @@ fn main() {
 
     let path = parse_input(&filename);
 
-    for p in path {
-        println!("{:?}", p);
+    let mut pos1 = Position{depth: 0, horizontal: 0, aim: 0};
+    let mut pos2 = Position{depth: 0, horizontal: 0, aim: 0};
+    
+    for leg in path {
+        pos1.mov_bad(&leg);
+        pos2.mov(&leg);
     }
 
+    println!("Final position BAD: {:?}", pos1);
+    println!("Product: {}", pos1.horizontal * pos1.depth);
 
+    println!("Final position GOOD: {:?}", pos2);
+    println!("Product: {}", pos2.horizontal * pos2.depth);
 }
