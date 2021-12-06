863f027ce83d2b1acaa46b6ceef36081310eb875
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2021-12-06 15:19:59 -0500

    3b! Finally!

--- 2021/day03/src/main.rs
+++ 2021/day03/src/main.rs
@@ -2,21 +2,6 @@ use std::env;
 use std::str;
 use std::fs;
 
-/*
-fn load_arr<T>(filename: &str) -> Vec<T> 
-    where T: str::FromStr { //, <T as str::FromStr>::Err: fmt::Debug {
-
-    let contents = fs::read_to_string(filename)
-        .expect("Couldn't read the file.");
-    let arr = contents.lines().map(
-                |line| match line.parse::<T>() {
-                        Ok(x) => x,
-                        _ => panic!("Couldn't parse val: {}", line)})
-                .collect();
-
-    arr
-}
-*/
 
 fn parse_input(filename: &str) -> (usize, Vec<u16>) {
     
@@ -33,7 +18,6 @@ fn parse_input(filename: &str) -> (usize, Vec<u16>) {
 fn count_bit(x: &[u16], b: usize) -> usize {
 
     let m: u16 = 1 << b;
-    println!("{}", m);
 
     let count = x.iter().fold(0, |c, val| c + ((val & m) >> b) as usize);
 
@@ -47,9 +31,7 @@ fn get_gamma_epsilon(x: &[u16], n: usize) -> (u32, u32) {
     let mut epsilon: u32 = 0;
 
     for b in 0..n {
-        println!("bit: {}", b);
         let c = count_bit(x, b);
-        println!("{}", c);
 
         let val: u16 = 1 << b;
 
@@ -64,6 +46,45 @@ fn get_gamma_epsilon(x: &[u16], n: usize) -> (u32, u32) {
     (gamma, epsilon)
 }
 
+fn get_oxygen_rating(x: &[u16], n: usize) -> u32 {
+   
+    let mut y: Vec<u16> = x.iter().cloned().collect();
+
+    for b in 0..n {
+        let c = count_bit(&y, n-b-1);
+        let m = (1 << n-b-1) as u16;
+        let target = if 2*c >= y.len() {m} else {0};
+        
+        y = y.into_iter().filter(|val| m & val == target).collect::<Vec<u16>>();
+
+        if y.len() == 1 {
+            break;
+        }
+    }
+    
+    y[0] as u32
+}
+
+fn get_co2_rating(x: &[u16], n: usize) -> u32 {
+   
+    let mut y: Vec<u16> = x.iter().cloned().collect();
+
+    for b in 0..n {
+        let c = count_bit(&y, n-b-1);
+        let m = (1 << n-b-1) as u16;
+        let target = if 2*c < y.len() {m} else {0};
+        
+        y = y.into_iter().filter(|val| m & val == target)
+                .collect::<Vec<u16>>();
+    
+        if y.len() == 1 {
+            break;
+        }
+    }
+    
+    y[0] as u32
+}
+
 
 fn main() {
 
@@ -73,8 +94,7 @@ fn main() {
 
     let (n, arr) = parse_input(filename);
 
-    println!("{:?}", arr);
-    println!("{}", n);
+    println!("entry size: {}", n);
 
     let (gamma, epsilon) = get_gamma_epsilon(&arr, n);
 
@@ -82,4 +102,12 @@ fn main() {
     println!("epsilon: {}", epsilon);
 
     println!("power: {}", gamma*epsilon);
+
+    let oxy = get_oxygen_rating(&arr, n);
+    let co2 = get_co2_rating(&arr, n);
+    
+    println!("oxygen: {}", oxy);
+    println!("c02: {}", co2);
+
+    println!("life support: {}", oxy*co2);
 }
