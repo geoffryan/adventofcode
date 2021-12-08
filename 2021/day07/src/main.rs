e8fd78e27e27bd96db04629a30a3051fac6b7780
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2021-12-08 15:28:48 -0500

    Hack!

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -1,3 +1,42 @@
+use std::env;
+use std::fs;
+
+fn fuel2(x: i32, s: i32) -> i32 {
+
+    let d = (x - s).abs();
+    (d*(d+1))/2
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(&filename).expect("Couldn't read file");
+
+    let mut positions: Vec<i32> = contents.trim().split(",")
+        .map(|x| x.parse().unwrap()).collect();
+
+    positions.sort();
+
+    let n = positions.len();
+
+    let median = positions[n/2];
+
+    let mut fuel = 0;
+    for x in positions.iter() {
+        fuel += (x-median).abs();
+    }
+
+    println!("Best Position: {}", median);
+    println!("Best Fuel Consumption: {}", fuel);
+
+    let s: Vec<i32> = (0..(*(positions.iter().max().unwrap()))).collect();
+
+    println!("Computing ALL the fuels.");
+    let F: Vec<i32> = s.iter().map(
+        |&si| positions.iter().fold(0, |tot, &x| tot + fuel2(x, si)))
+        .collect();
+
+    println!("Finding the min.");
+    println!("Min fuel: {}", F.iter().min().unwrap());
 }
