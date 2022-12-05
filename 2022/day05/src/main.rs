40ca5e03300e05dad10aeae860a578b71a71b841
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2022-12-05 16:23:16 -0500

    day05 complete

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,5 +1,60 @@
 mod util;
 
+struct Stacks {
+    crates: Vec<Vec<char>>
+}
+
+impl Stacks {
+    fn from(config: &Vec<Vec<char>>) -> Stacks {
+        let depth = config.len();
+        let n = (config[0].len() + 1) / 4;
+
+        let mut crates: Vec<Vec<char>> = Vec::new();
+        for _ in 0..n {
+            crates.push(Vec::new());
+        }
+
+        for i in (0..(depth-1)).rev() {
+            for j in 0..n {
+                let idx = 4*j+1;
+                
+                if config[i][idx] == ' ' {
+                    continue;
+                }
+
+                crates[j].push(config[i][idx]);
+            }
+        }
+
+        Stacks { crates }
+    }
+
+    fn use_crane_9000(&mut self, num: usize, from: usize, to: usize) {
+        for _ in 0..num {
+            let c = self.crates[from-1].pop().unwrap();
+            self.crates[to-1].push(c);
+        }
+    }
+
+    fn use_crane_9001(&mut self, num: usize, from: usize, to: usize) {
+        let mut buf = Vec::new();
+
+        for _ in 0..num {
+            buf.push(self.crates[from-1].pop().unwrap());
+        }
+        for _ in 0..num {
+            self.crates[to-1].push(buf.pop().unwrap());
+        }
+    }
+
+    fn top_crates(&self) -> String {
+
+        self.crates.iter()
+            .map(|stack| stack[stack.len()-1])
+            .collect::<String>()
+    }
+}
+
 fn main() {
     let input = util::get_input(2022, 5);
 
@@ -14,19 +69,32 @@ fn main() {
         line = lines.next().unwrap();
     }
 
-    println!("{:?}", stack_config);
 
     for line in lines {
-        println!("{}", line);
-        let mut words = line.trim().split_whitespace();
-        println!("{:?}", words);
-        let num = words.nth(1).unwrap().parse::<i64>().unwrap();
-        let from = words.nth(3).unwrap().parse::<i64>().unwrap();
-        let to = words.nth(5).unwrap().parse::<i64>().unwrap();
-        for _ in 0..num {
-            move_list.push((from, to));
-        }
+        let words: Vec<&str> = line.trim().split_whitespace().collect();
+        let num = words[1].parse::<usize>().unwrap();
+        let from = words[3].parse::<usize>().unwrap();
+        let to = words[5].parse::<usize>().unwrap();
+        move_list.push((num, from, to));
     }
-    println!("{:?}", move_list);
 
+    let mut stacks = Stacks::from(&stack_config);
+
+    for &(num, from, to) in move_list.iter() {
+        stacks.use_crane_9000(num, from, to);
+    }
+
+    let tops = stacks.top_crates();
+
+    println!("Top crates 9000 are: {}", tops);
+
+    stacks = Stacks::from(&stack_config);
+
+    for &(num, from, to) in move_list.iter() {
+        stacks.use_crane_9001(num, from, to);
+    }
+
+    let tops = stacks.top_crates();
+
+    println!("Top crates 9001 are: {}", tops);
 }
